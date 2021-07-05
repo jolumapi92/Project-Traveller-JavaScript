b2e28db544fbcbd2dcbd7516a3ba59975d9ac78d
Jose Matias <josematias92@gmail.com>
2021-07-05 18:27:44 -0500

    changed

--- frontend/src/components/sideBarActivities.js
+++ frontend/src/components/sideBarActivities.js
@@ -8,8 +8,8 @@ const EventActivities = () => {
 
     return ( 
         <div className="side-bar-activities">
-            <h3 className="border-bottom border-warning border-4">Activities</h3> 
-            <p>These are all the activities you can choose from according to the location you selected.</p> 
+            <h3 className="border-bottom border-warning border-4 title-header-activities-location">Activities</h3> 
+            <p className="instructions-of-activities-event">These are all the activities you can choose from according to the location you selected.</p> 
             {loading && loading }
             <div className="all-activities-container">
                 { activities && activities.map(activity => {return <div className="activities-from-location"> <p> <strong>Concept:</strong> {activity.name} </p> <p> <strong>Description:</strong>  {activity.description}</p> <p> <strong>Category:</strong> {activity.category}</p> </div> }) }
