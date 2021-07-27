1d6db86ca121f01c85fdfc0ce119fb4d47845479
Jose Matias <josematias92@gmail.com>
2021-07-26 22:04:37 -0500

    changed

--- frontend/src/components/allBookings.js
+++ frontend/src/components/allBookings.js
@@ -1,7 +1,6 @@
 import useFetch from './useFetch';
 import { Link } from 'react-router-dom';
 import React from 'react';
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 
@@ -17,16 +16,7 @@ const AllBookings = () => {
         } catch (error) {
             
         }
-        
     }, [events])
-        
-   
-
-    let [ retrieve, setRetrieve ] = useState(null);
-    const [ loadingData, setLoadingData ] = useState(null);
-    const [ background1, setBackground1 ] = useState(null);
-    const [ background2, setBackground2 ] = useState(null);
-    const [ background3, setBackground3 ] = useState(null);
     
 
     // const handleClickTicket = async (id) => {
@@ -62,7 +52,6 @@ const AllBookings = () => {
     return ( 
         <section className="p-5 d-flex align-items-center justify-content-center main-section-allbookings">
             { loading && loading }
-            { loadingData && loadingData }
             { reversedArrayEvents && reversedArrayEvents.map( element => { return <Link to={`/Allbookings/${element._id}`}><div className="card-event my-4"> <p className="destination-ticket"> Destination: <strong> {element.location} </strong> </p> <p className="agent-name"> agent assigned: {element.agent.username}</p> <p className="traveller-name"> Traveller: {element.traveller.name}</p> <p className="number-of-passengers"> Passengers: {element.number}</p> <p className="purchased-date">booked: {element.createdAt}</p> <p className="unique-id">Vagabond Code: {element._id}</p> <p className="boarding-pass">Boarding Pass</p> <p className="type-traveller">Premium Class</p> <Link to={`/MyJourney/${element._id}`}  className="button-journey"><button className="btn button-journey">Activities</button></Link> </div></Link>} ) }
             { error && error }
         </section>
